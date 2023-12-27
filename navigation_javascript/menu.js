00caa4574aeb3873da351130d2b26aa699dfb9ed
truong <lst27062004@gmail.com>
2023-12-28 02:12:21 +0700

    ban tum lum

--- navigation_javascript/menu.js
+++ navigation_javascript/menu.js
@@ -1,7 +1,8 @@
+var menu = document.querySelector('.menuroi');
+var content = document.querySelector('.noidung');
+var body = document.querySelector('body');
+var overlay = document.querySelector(".nen_mo");
 document.addEventListener('DOMContentLoaded', function() {
-    var menu = document.querySelector('.menuroi');
-    var content = document.querySelector('.noidung');
-    var body = document.querySelector('body');
 
     menu.addEventListener('click', function(event) {
         event.stopPropagation();
@@ -10,13 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isDisplay === 'none') {
             content.style.animation= 'side_right 3s ease-in-out forwards';
             content.style.display = 'block';
+            overlay.style.display = 'block';
+            overlay.style.animation = "overlay 1s ease-in forwards";
             
         } if (isDisplay == 'block') {
             content.style.display = 'none';
+            overlay.style.display = 'none';
         }
     });
 
     body.addEventListener('click', function() {
         content.style.display = 'none';
+        overlay.style.display = 'none';
     });
 });
--- navigation_javascript/promotion.js
+++ navigation_javascript/promotion.js
@@ -12,3 +12,4 @@ menuroiE.addEventListener("click", ()=>{
 
     }
 })
+
