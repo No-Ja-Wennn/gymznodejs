42ddebefd6180f2d64540f9a30e7ded80caf80f5
truong <lst27062004@gmail.com>
2023-12-28 03:54:00 +0700

    code menu den noi bi tri

--- navigation_javascript/menu.js
+++ navigation_javascript/menu.js
@@ -2,6 +2,7 @@ var menu = document.querySelector('.menuroi');
 var content = document.querySelector('.noidung');
 var body = document.querySelector('body');
 var overlay = document.querySelector(".nen_mo");
+var logo = document.querySelector(".header__inner__logo a")
 document.addEventListener('DOMContentLoaded', function() {
 
     menu.addEventListener('click', function(event) {
@@ -9,19 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
         var isDisplay = getComputedStyle(content).display;
 
         if (isDisplay === 'none') {
-            content.style.animation= 'side_right 3s ease-in-out forwards';
+            content.style.animation = 'side_right 0.4s ease-in-out forwards';
             content.style.display = 'block';
             overlay.style.display = 'block';
-            overlay.style.animation = "overlay 1s ease-in forwards";
+            overlay.style.animation = "overlay 0.5s ease-in forwards";
+            logo.style.display = 'none';
             
         } if (isDisplay == 'block') {
             content.style.display = 'none';
             overlay.style.display = 'none';
-        }
+            logo.style.display = 'block';
+        } 
     });
 
     body.addEventListener('click', function() {
         content.style.display = 'none';
         overlay.style.display = 'none';
+        logo.style.display = 'block';
     });
 });
