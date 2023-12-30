308323ad35c6d2d06593da2ca544e492a42310de
No-Ja-Wennn <ngogiaquyendhtn223@gmail.com>
2023-12-30 18:29:40 +0700

    caculator bmi ok

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const fullnameElement = document.getElementById("fullname")
 const genderElement = document.getElementById("gender")
 const phoneElement = document.getElementById("phone")
 const changeSubmitElement = document.querySelector(".change__submit")
-const elements = document.querySelectorAll(".BMI__value__row__data");
+const BMIDataArray = document.querySelectorAll(".BMI__value__row__data");
+const errorMessageOfBMIE = document.querySelector(".change__input__error");
 
 function calculateBMI(heightCm, weight) {
     if (heightCm > 0 && weight > 0) {
@@ -21,33 +22,73 @@ function calculateBMI(heightCm, weight) {
 }
 
 function innerValueToForm(height = 0, weight = 0, old = 0, gender = "khác", BMINumber = 0, statusOfHeath = "", message = "") {
-    for (var i = 0; i < elements.length; i++) {
+    height = height / 100;
+    for (var i = 0; i < BMIDataArray.length; i++) {
         switch (i) {
             case 0:
-                elements[i].innerHTML = `${height}m`; // Chiều cao
+                BMIDataArray[i].innerHTML = `${height}m`; // Chiều cao
                 break;
             case 1:
-                elements[i].innerHTML = `${weight}kg`; // Cân nặng
+                BMIDataArray[i].innerHTML = `${weight}kg`; // Cân nặng
                 break;
             case 2:
-                elements[i].innerHTML = old; // Tuổi
+                BMIDataArray[i].innerHTML = old; // Tuổi
                 break;
             case 3:
-                elements[i].innerHTML = gender; // Giới tính
+                BMIDataArray[i].innerHTML = gender; // Giới tính
                 break;
             case 4:
-                elements[i].innerHTML = BMINumber; // Chỉ số BMI
+                BMIDataArray[i].innerHTML = BMINumber; // Chỉ số BMI
                 break;
             case 5:
-                elements[i].innerHTML = statusOfHeath; // Chỉ số BMI
+                BMIDataArray[i].innerHTML = statusOfHeath; // Chỉ số BMI
                 break;
             case 6:
-                elements[i].innerHTML = message; // Chỉ số BMI
+                BMIDataArray[i].innerHTML = message; // Chỉ số BMI
                 break;
             default:
                 break;
         }
     }
+}
+
+
+function validatePhoneNumber(phoneNumber) {
+    var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    return regex.test(phoneNumber);
+}
+
+function errorNotification(heightElement, weightElement, ageElement, phoneElement) {
+    var heightValue = heightElement.value;
+    var weightValue = weightElement.value;
+    var ageValue = ageElement.value;
+    var phoneValue = phoneElement.value;
+
+    heightElement.style.borderColor = "#000";
+    weightElement.style.borderColor = "#000";
+    ageElement.style.borderColor = "#000";
+    phoneElement.style.borderColor = "#000";
+    if (heightValue < 0 || heightValue == "" || isNaN(heightValue)) {
+        errorMessageOfBMIE.innerText = "Vui lòng nhập chiều cao của bạn"
+        heightElement.style.borderColor = "#ff0436";
+        return false;
+    }
+    if (weightValue < 0 || weightValue == "" || isNaN(weightValue)) {
+        errorMessageOfBMIE.innerText = "Vui lòng nhập cân nặng của bạn"
+        weightElement.style.borderColor = "#ff0436";
+        return false;
+    }
+    if (ageValue < 0 || ageValue == "" || isNaN(ageValue)) {
+        errorMessageOfBMIE.innerText = "Vui lòng nhập tuổi của bạn"
+        ageElement.style.borderColor = "#ff0436";
+        return false;
+    }
+    if (phoneValue < 0 || phoneValue == "" || isNaN(phoneValue) || !validatePhoneNumber(phoneValue)) {
+        errorMessageOfBMIE.innerText = "Vui lòng nhập đúng số điện thoại"
+        phoneElement.style.borderColor = "#ff0436";
+        return false;
+    }
+    return true;
 }
 
 changeSubmitElement.addEventListener("click", function () {
@@ -60,30 +101,43 @@ changeSubmitElement.addEventListener("click", function () {
     var statusOfHeath = "";
     var message = "";
     var BMI = calculateBMI(heightValue, weightValue);
-    console.log(BMI)
-    if (BMI == -1) {
-        console.log("Vui lòng nhập cân nặng và chiều cao lớn hơn 0")
-    } else if (BMI < 18.5) {
-        statusOfHeath = "Bình thường";
-        message = "Bạn có một cơ thể tốt";
-    } else if (BMI >= 18.5 && BMI < 25) {
-        statusOfHeath = "Thiếu cân";
-        message = "Bạn cần áp dụng chế độ ăn và thể thao để tăng cân";
-    } else if (BMI >= 25 && BMI < 30) {
-        statusOfHeath = "Thừa cân";
-        message = "Bạn cần lên kế hoạch giảm cân";
-    } else if (BMI >= 30) {
-        statusOfHeath = "Béo phì";
-        message = "Bạn cần tìm hiểu và áp dụng các biện pháp giảm cân mạnh mẽ hơn";
+
+    if (errorNotification(heightElement, weightElement, ageElement, phoneElement)) {
+        if (BMI == -1) {
+            console.log("Hello")
+        } else if (BMI < 18.5) {
+            statusOfHeath = "Bình thường";
+            message = "Bạn có một cơ thể tốt";
+        } else if (BMI >= 18.5 && BMI < 25) {
+            statusOfHeath = "Thiếu cân";
+            message = "Bạn cần áp dụng chế độ ăn và thể thao để tăng cân";
+        } else if (BMI >= 25 && BMI < 30) {
+            statusOfHeath = "Thừa cân";
+            message = "Bạn cần lên kế hoạch giảm cân";
+        } else if (BMI >= 30) {
+            statusOfHeath = "Béo phì";
+            message = "Bạn cần tìm hiểu và áp dụng các biện pháp giảm cân mạnh mẽ hơn";
+        }
+        if (BMI != -1) {
+            innerValueToForm(heightValue, weightValue, ageValue, genderValue, BMI, statusOfHeath, message)
+            modalElement.style.display = "flex";
+        }
     }
-    innerValueToForm(heightValue, weightValue, ageValue, genderValue, BMI, statusOfHeath, message)
-    modalElement.style.display = "flex";
 })
 
 const bmiIconExit = document.querySelector(".BMI__title__icon");
 
-bmiIconExit.addEventListener("click", function(){
+
+const inputBox = document.querySelector(".change__input");
+const inputArray = inputBox.getElementsByTagName("input");
+
+
+bmiIconExit.addEventListener("click", function () {
     modalElement.style.display = "none";
+    for(i = 0; i < inputArray.length; i++){
+        inputArray[i].value = "";
+    }
+
 })
 
 
