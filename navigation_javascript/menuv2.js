58ac2b8ddf016ed2b99376815b035c11288ba182
vannguyen <vannguyen1907.pb@gmail.com>
2024-01-08 14:02:05 +0700

    edit

--- navigation_javascript/menuv2.js
+++ navigation_javascript/menuv2.js
@@ -4,11 +4,11 @@ var body = document.querySelector('body');
 var overlay = document.querySelector(".nen_mo");
 var logo = document.querySelector(".header__inner__logo a")
 document.addEventListener('DOMContentLoaded', function() {
-
     menu.addEventListener('click', function(event) {
         event.stopPropagation();
         var isDisplay = getComputedStyle(content).display;
-
+        
+        console.log(getComputedStyle(content).display)
         if (isDisplay === 'none') {
             
             content.style.animation = 'slide_right 0.4s ease-in-out forwards';
@@ -16,34 +16,34 @@ document.addEventListener('DOMContentLoaded', function() {
             overlay.style.animation = "in_opacity 0.4s ease-in forwards";
             overlay.style.display = 'block';
             logo.style.display = 'none';
-          
+            
         } else {
-        // } if (isDisplay === 'block') {
-            content.style.animation = 'slide_left 0.4s ease-in-out forwards';
-            // content.style.display = 'none';
-            overlay.style.animation = 're_opacity 0.4s ease-in forwards';
+            // } if (isDisplay === 'block') {
+                content.style.animation = 'slide_left 0.4s ease-in-out forwards';
+                // content.style.display = 'none';
+                overlay.style.animation = 're_opacity 0.4s ease-in forwards';
             // overlay.style.display = 'none';
             logo.style.display = 'block';
         } 
     });
-    menu.addEventListener('touchstart', function(event) {
-        event.stopPropagation();
-        var isDisplay = getComputedStyle(content).display;
+    // menu.addEventListener('touchstart', function(event) {
+    //     event.stopPropagation();
+    //     var isDisplay = getComputedStyle(content).display;
     
-        if (isDisplay === 'none') {
-            content.style.animation = 'slide_right 0.4s ease-in-out forwards';
-            content.style.display = 'block';
-            overlay.style.animation = "in_opacity 0.4s ease-in forwards";
-            overlay.style.display = 'block';
-            logo.style.display = 'none';
-        } else {
-            content.style.animation = 'slide_left 0.4s ease-in-out forwards';
-            // content.style.display = 'none';
-            overlay.style.animation = 're_opacity 0.4s ease-in forwards';
-            // overlay.style.display = 'none';
-            logo.style.display = 'block';
-        }
-    });
+    //     if (isDisplay === 'none') {
+    //         content.style.animation = 'slide_right 0.4s ease-in-out forwards';
+    //         content.style.display = 'block';
+    //         overlay.style.animation = "in_opacity 0.4s ease-in forwards";
+    //         overlay.style.display = 'block';
+    //         logo.style.display = 'none';
+    //     } else {
+    //         content.style.animation = 'slide_left 0.4s ease-in-out forwards';
+    //         // content.style.display = 'none';
+    //         overlay.style.animation = 're_opacity 0.4s ease-in forwards';
+    //         // overlay.style.display = 'none';
+    //         logo.style.display = 'block';
+    //     }
+    // });
     body.addEventListener('click', function() {
         content.style.animation = 'slide_left 0.4s ease-in-out forwards';
         // content.style.display = 'none';
