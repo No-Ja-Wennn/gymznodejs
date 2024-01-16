6a0bb822f635d1e24865b42becee1fff403453b3
No-Ja-Wennn <ngogiaquyendhtn223@gmail.com>
2024-01-16 22:13:06 +0700

    fixx

--- navigation_javascript/admin.js
+++ navigation_javascript/admin.js
@@ -30,18 +30,19 @@ if (!localStorage.getItem('cartData')) {
 
 
 function innerTableCart(array) {
-    const tableCart = showCart.getElementsByTagName("table")[0];
+    const tableCart = showCart.querySelector(".table__show");
     array.map(value => {
         var trElement = document.createElement("tr");
+        trElement.className = "tr__show";
         trElement.innerHTML = `
-        <td>${value.id}</td>
-        <td>${value.name}</td>
-        <td>${value.age}</td>
-        <td>${value.phoneNumber}</td>
-        <td>${value.cartType}</td>
-        <td>${value.dateStart}</td>
-        <td>${value.dateEnd}</td>
-        <td>
+        <td class="td__show">${value.id}</td>
+        <td class="td__show">${value.name}</td>
+        <td class="td__show">${value.age}</td>
+        <td class="td__show">${value.phoneNumber}</td>
+        <td class="td__show">${value.cartType}</td>
+        <td class="td__show">${value.dateStart}</td>
+        <td class="td__show">${value.dateEnd}</td>
+        <td class="td__show">
         <i class="fa-solid fa-pen-to-square" onclick="editCart(this)"></i>
         <i class="fa-solid fa-trash" onclick="removeCart(this)"></i>
         </td>
@@ -60,18 +61,18 @@ let trAddArray2;
 btnAddCart.addEventListener("click", () => {
     if (activeOption == "") {
         activeOption = "add";
-        const tableCart = showCart.getElementsByTagName("table")[0];
+        const tableCart = showCart.querySelector(".table__show");
         var trElement = document.createElement("tr");
-        trElement.className = "trAdd";
+        trElement.className = "trAdd tr__show";
         trElement.innerHTML = `
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td><input class="inputAdd" type="text" placeholder="....."></td>
-        <td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show"><input class="input__show inputAdd" type="text" placeholder="....."></td>
+        <td class="td__show">
             <i class="fa-solid fa-pen-to-square" onclick="editCart(this)"></i>
             <i class="fa-solid fa-trash" onclick="removeCart(this)"></i>
         </td>
@@ -80,11 +81,11 @@ btnAddCart.addEventListener("click", () => {
         trAddArray = document.querySelectorAll(".trAdd");
         trAddArray = Array.from(trAddArray);
     } else if (activeOption == "add") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác này";
+        showErrorToast("Bạn đang thực hiện thao tác này");
     } else if (activeOption == "edit") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác chỉnh sửa thẻ, bấm lưu để tiếp tục sử dụng chức năng này";
+        showErrorToast("Bạn đang thực hiện thao tác chỉnh sửa thẻ, bấm lưu để tiếp tục sử dụng chức năng này");
     } else if (activeOption == "remove") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác xóa thẻ, bấm lưu để tiếp tục sử dụng chức năng này";
+        showErrorToast("Bạn đang thực hiện thao tác xóa thẻ, bấm lưu để tiếp tục sử dụng chức năng này");
     }
 
 })
@@ -114,17 +115,18 @@ saveCartElement.addEventListener("click", () => {
                 arrayData.map(value => {
                     for (var i = 0; i < dataCompear.length; i++) {
                         if (dataCompear[i].id == value[0]) {
-                            saveMessage.innerText = "ID trùng lặp!"
+                            showErrorToast("ID trùng lặp!");
                             return;
                         }
                     }
+
                     if (isNaN(value[2])) {
-                        saveMessage.innerText = "Tuổi là số!";
+                        showErrorToast("Tuổi là số!");
                         return;
                     }
                     var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
                     if (!regex.test(value[3])) {
-                        saveMessage.innerText = "Nhập đúng số điện thoại!";
+                        showErrorToast("Nhập đúng số điện thoại!");
                         return;
                     }
                     data = {
@@ -141,7 +143,7 @@ saveCartElement.addEventListener("click", () => {
                     localStorage.setItem('cartData', JSON.stringify(myData));
                     var trElement = document.querySelector(".trAdd");
                     trElement.classList.remove("trAdd");
-                    saveMessage.innerText = "Thêm thành công!";
+                    showSuccessToast("Thêm thành công!");
                     activeOption = "";
                 })
                 // if (test == false)
@@ -149,10 +151,11 @@ saveCartElement.addEventListener("click", () => {
 
 
             } else {
-                saveMessage.innerText = "Vui lòng điền đầy đủ thông tin"
+                showErrorToast("Vui lòng điền đầy đủ thông tin");
+
             }
         } else {
-            saveMessage.innerText = "Không có gì thay đổi!"
+            showErrorToast("Không có gì thay đổi!");
         }
     } else if (activeOption == "edit") {
         // trelementChange
@@ -162,22 +165,24 @@ saveCartElement.addEventListener("click", () => {
         var valueArray = [];
         for (var i = 0; i < tdArray.length - 1; i++) {
             var value = tdArray[i].querySelector(".inputAdd").value;
-            if (value)
+            if (value != ""){
+                console.log("hello")
                 valueArray[i] = value;
+            }
             else
                 valueArray[i] = tdArray[i].querySelector(".inputAdd").getAttribute("placeholder")
         }
-
         if (checkInformationValid(valueArray)) {
             for (var i = 0; i < tdArray.length - 1; i++) {
                 tdArray[i].innerHTML = `${valueArray[i]}`
             }
+        }else{
+            return;
         }
-        // Tạo một đối tượng data mới từ mảng giá trị
         var data = {
             "id": `${valueArray[0]}`,
             "name": `${valueArray[1]}`,
-            "age": `${valueArray[2]}`,
+            "age": `${Number(valueArray[2])}`,
             "phoneNumber": `${valueArray[3]}`,
             "cartType": `${valueArray[4]}`,
             "dateStart": `${valueArray[5]}`,
@@ -196,16 +201,16 @@ saveCartElement.addEventListener("click", () => {
 
         var trElement = document.querySelector(".trEdit");
         trElement.classList.remove("trEdit");
-        saveMessage.innerText = "Sửa thành công!";
+        showSuccessToast("Sửa thành công!")
         activeOption = "";
     } else if (activeOption == "remove") {
 
-
+        console.log(dataAfterRemove)
         localStorage.setItem('cartData', JSON.stringify(dataAfterRemove));
-        saveMessage.innerText = "Xóa thành công!";
+        showSuccessToast("Xóa thành công!");
         activeOption = "";
     } else {
-        saveMessage.innerText = "Vui lòng chọn chức năng!"
+        showErrorToast("Vui lòng chọn chức năng!");
     }
 
 })
@@ -214,26 +219,36 @@ function checkInformationValid(arrayData) {
     myData = JSON.parse(localStorage.getItem('cartData'));
     dataCompear = myData.carts;
     var test = true;
-    arrayData.map(value => {
-        for (var i = 0; i < dataCompear.length; i++) {
-            if (dataCompear[i].id == value[0]) {
-                saveMessage.innerText = "ID trùng lặp!"
-                test = false;
-                return;
-            }
+    arrayData[2] = Number(arrayData[2])
+    var arrayChange = [];
+
+    for (var i = 0; i < dataCompear.length; i++) {
+        arrayChange[i] = [
+            dataCompear[i].id,
+            dataCompear[i].name,
+            Number(dataCompear[i].age),
+            dataCompear[i].phoneNumber,
+            dataCompear[i].cartType,
+            dataCompear[i].dateStart,
+            dataCompear[i].dateEnd
+        ]
+        if (arraysEqual(arrayChange[i], arrayData)) {
+            continue;
         }
-        if (isNaN(value[2])) {
-            saveMessage.innerText = "Tuổi là số!";
-            test = false
-            return;
-        }
-        var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-        if (!regex.test(value[3])) {
-            saveMessage.innerText = "Nhập đúng số điện thoại!";
+        if (dataCompear[i].id == arrayData[0]) {
+            showErrorToast("ID trùng lặp!");
             test = false;
-            return;
         }
-    });
+    }
+    if (isNaN(arrayData[2])) {
+        showErrorToast("Tuổi là số!");
+        test = false
+    }
+    var regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (!regex.test(arrayData[3])) {
+        showErrorToast("Nhập đúng số điện thoại!");
+        test = false;
+    }
     return test;
 }
 
@@ -247,6 +262,22 @@ function checkAll(trArray) {
         }
     })
     return ktra;
+}
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // Nếu bạn không quan tâm đến việc duyệt qua các phần tử theo thứ tự,
+    // thì bạn có thể sắp xếp mảng này trước khi so sánh.
+    // Nếu bạn quan tâm đến thứ tự, thì hãy bỏ qua bước này.
+    // a.sort(); b.sort();
+
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
 }
 
 function checkRowAddCart(arrayElement) {
@@ -270,8 +301,8 @@ function editCart(thisElement) {
         var tableElement = trElement.parentElement;
         var trAray = tableElement.querySelectorAll("tr");
         trAray = Array.from(trAray);
-        trAray.map((value, index)=>{
-            if(value === trElement){
+        trAray.map((value, index) => {
+            if (value === trElement) {
                 indexChange = index - 1;
             }
         })
@@ -288,19 +319,18 @@ function editCart(thisElement) {
             tdArray[i].innerHTML = `<input class="inputAdd" type="text" placeholder="${valueArray[i]}">`
         }
     } else if (activeOption == "edit") {
-        saveMessage.innerText = "vui lòng bấm lưu trước khi sửa dòng khác!";
+        showErrorToast("vui lòng bấm lưu trước khi sửa dòng khác!");
     } else if (activeOption == "remove") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác xóa, bấm lưu để tiếp tục thực hiện chức năng này!";
+        showErrorToast("Bạn đang thực hiện thao tác xóa, bấm lưu để tiếp tục thực hiện chức năng này!");
     } else if (activeOption == "add") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác thêm thẻ, hãy hoàn tất chức năng này trước!";
+        showErrorToast("Bạn đang thực hiện thao tác thêm thẻ, hãy hoàn tất chức năng này trước!");
     }
 }
 
 let dataAfterRemove;
 
 function removeCart(thisElement) {
-    console.log(activeOption)
-    if (activeOption == "" || activeOption == "remove") {
+    if (activeOption == "" || activeOption == "remove" || activeOption == "add") {
         activeOption = "remove";
         var trElement = thisElement.parentElement.parentElement;
         trelementChange = trElement;
@@ -334,19 +364,164 @@ function removeCart(thisElement) {
         if (index !== -1) {
             myData.carts.splice(index, 1);
         }
-
         dataAfterRemove = myData;
     } else if (activeOption == "edit") {
-        saveMessage.innerText = "Bạn đang thực hiện thao tác chỉnh sửa, bấm lưu để tiếp tục thực hiện chức năng này!";
+        showErrorToast("Bạn đang thực hiện thao tác chỉnh sửa, bấm lưu để tiếp tục thực hiện chức năng này!");
     } else if (activeOption == "add") {
         var trElement = thisElement.parentElement.parentElement;
-        if (trElement.className == "trAdd") {
+        console.log(trElement)
+        if (trElement.className == "trAdd tr__show") {
             activeOption = "remove";
             trelementChange = trElement;
             trElement.remove();
             activeOption == ""
         }
-
-        saveMessage.innerText = "Bạn đang thực hiện thao tác thêm thẻ, hãy hoàn tất chức năng này trước!";
+        // showErrorToast("Bạn đang thực hiện thao tác thêm thẻ, hãy hoàn tất chức năng này trước!");
     }
+}
+
+
+function findByID(thisElement) {
+    myData = JSON.parse(localStorage.getItem('cartData'));
+    dataArray = myData.carts;
+    const findBox = document.querySelector(".show__find__id__box");
+    findBox.innerText = ""
+    if (thisElement.value == "") {
+        findBox.innerText = "";
+    } else {
+        dataArray.map((value, index) => {
+            if (value.id.toLowerCase().includes(thisElement.value.toLowerCase())) {
+                var spanE = document.createElement("show__find_id");
+                spanE.className = "show__find_id";
+                spanE.innerText = value.id;
+                findBox.appendChild(spanE);
+            }
+        });
+    }
+
+}
+
+const findBTN = document.querySelector(".container__show__cart__find__btn");
+findBTN.addEventListener("click", () => {
+    var inputID = document.querySelector(".show__input__id").value;
+    var selectPackage = document.querySelector(".show__input__package").value;
+    var inputStart = document.getElementById("input-date-start").value
+    var inputEnd = document.getElementById("input-date-end").value;
+
+    myData = JSON.parse(localStorage.getItem('cartData'));
+    dataArray = myData.carts;
+    dataArray.map((value, index) => {
+        var test = true;
+        if (inputID != "" && test == true) {
+            if (inputID !== value.id) {
+                test = false;
+            }
+        }
+        if (selectPackage != "" && test == true) {
+            if (selectPackage !== value.cartType) {
+                test = false;
+            }
+        }
+        if (inputStart != "" && test == true) {
+            if (inputStart !== value.dateStart) {
+                test = false;
+            }
+        }
+        if (inputEnd != "" && test == true) {
+            if (inputEnd !== value.dateEnd) {
+                test = false;
+            }
+        }
+
+        if (test == true) {
+            console.log(test);
+            console.log(value);
+            console.log("tìm thấy");
+        }
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// toast
+function toast({
+    title = "",
+    message = '',
+    type = "info",
+    duration = 3000
+}) {
+    const main = document.getElementById('toast');
+    if (main) {
+        const toast = document.createElement('div');
+
+        // auto remove
+        const autoRemove = setTimeout(function () {
+            main.removeChild(toast)
+        }, duration + 1000)
+        // remove when click
+        toast.onclick = function (e) {
+            if (e.target.closest('.toast__close')) {
+                main.removeChild(toast);
+                clearTimeout(autoRemove);
+            }
+        }
+        const icons = {
+            success: ' fas fa-circle-check',
+            info: 'fas fa-info-circle',
+            warning: 'fas fa-exclamation-circle',
+            error: 'fas fa-exclamation-circle',
+
+        }
+        const icon = icons[type]
+
+        const delay = (duration / 1000).toFixed(2)
+        toast.classList.add('toast', `toast__${type}`);
+        toast.style.animation = `slideInleft ease .3s, fadeOut linear 1s ${delay}s forwards`
+        toast.innerHTML = `
+            <div class="toast__icon">
+                <i class="${icon}"></i>
+            </div>
+            <div class="toast__body">
+                <div class="toast__title">${title}</div>
+                <div class="toast__msg">${message}</div>
+            </div>
+            <div class="toast__close">
+                <i class="fa-sharp fa-solid fa-xmark"></i>
+            </div>
+                    `;
+        main.appendChild(toast);
+        setTimeout(function () {
+            // console.log(main)
+            // main.removeChild(toast)
+        }, duration + 1000)
+    }
+}
+function showSuccessToast(message) {
+    toast({
+        title: "Thành công",
+        message: message,
+        type: "success",
+        duration: 5000,
+    })
+}
+function showErrorToast(message) {
+    toast({
+        title: "Thất bại",
+        message: message,
+        type: "error",
+        duration: 5000,
+    })
 }
