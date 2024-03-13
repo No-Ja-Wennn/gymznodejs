c0d9725115fa5d1dbcbd338f9f3dfd5a3cef3b7a
truong <lst27062004@gmail.com>
2024-03-13 22:46:43 +0700

    peronal trainer 82%

--- navigation_javascript/personal_trainer.js
+++ navigation_javascript/personal_trainer.js
@@ -49,3 +49,85 @@ if(cancel)
 cancel.addEventListener('click', function(){
     form.style.display = 'none';
 })
+
+
+var button3 = document.getElementById('button3');
+var button4 = document.getElementById('button4');
+var button5 = document.getElementById('button5');
+var button6 = document.getElementById('button6');
+var button7 = document.getElementById('button7');
+var if3 = document.querySelector('#if3');
+var if4 = document.querySelector('#if4');
+var if5 = document.querySelector('#if5');
+var if6 = document.querySelector('#if6');
+var if7 = document.querySelector('#if7');
+var cancel3 = document.querySelector('#cancel3 i');
+var cancel4 = document.querySelector('#cancel4 i');
+var cancel5 = document.querySelector('#cancel5 i');
+var cancel6 = document.querySelector('#cancel6 i');
+var cancel7 = document.querySelector('#cancel7 i');
+var body = document.querySelector('body');
+
+if(button3)
+button3.addEventListener('click', function(){
+    if3.style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
+if(cancel3)
+cancel3.addEventListener('click', function(){
+    if3.style.display = 'none';
+    body.style.overflow = 'auto';
+})
+
+
+if(button4)
+button4.addEventListener('click', function(){
+    if4.style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
+if(cancel4)
+cancel4.addEventListener('click', function(){
+    if4.style.display = 'none';
+    body.style.overflow = 'auto';
+})
+
+
+if(button5)
+button5.addEventListener('click', function(){
+    if5.style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
+if(cancel5)
+cancel5.addEventListener('click', function(){
+    if5.style.display = 'none';
+    body.style.overflow = 'auto';
+})
+
+
+if(button6)
+button6.addEventListener('click', function(){
+    if6.style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
+if(cancel6)
+cancel6.addEventListener('click', function(){
+    if6.style.display = 'none';
+    body.style.overflow = 'auto';
+})
+
+
+if(button7)
+button7.addEventListener('click', function(){
+    if7.style.display = 'flex';
+    body.style.overflow = 'hidden';
+});
+
+if(cancel7)
+cancel7.addEventListener('click', function(){
+    if7.style.display = 'none';
+    body.style.overflow = 'auto';
+})
