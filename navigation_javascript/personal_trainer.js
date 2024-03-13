5800e5232aa3f9244a89a16ccda473960c94a264
No-Ja-Wenn <ngogiaquyendhtn223@gmail.com>
2024-03-14 00:10:39 +0700

    fix show tranner

--- navigation_javascript/personal_trainer.js
+++ navigation_javascript/personal_trainer.js
@@ -1,6 +1,12 @@
 var slickSlider = $('.image__slider');
 
-$(document).ready(function(){
+
+// Lưu trữ các id trước khi khởi tạo Slick Slider
+// var ids = slickSlider.find('[id]').map(function () {
+//     return this.id;
+// });
+
+$(document).ready(function () {
     // Khởi tạo Slick Slider ban đầu
     slickSlider.slick({
         infinite: true,
@@ -11,8 +17,8 @@ $(document).ready(function(){
         draggable: false,
         pauseOnFocus: false,
         pauseOnHover: false,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left' aria-hidden='true'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>"
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></button>"
     });
 
     function updateSlider() {
@@ -29,6 +35,12 @@ $(document).ready(function(){
 
     // Gọi hàm cập nhật khi tải trang và khi thay đổi kích thước màn hình
     $(window).on('load resize', updateSlider);
+
+    // Đặt lại các id sau khi Slick Slider đã được khởi tạo
+    // slickSlider.find('[id]').each(function (index) {
+    //     this.id = ids[index];
+    // });
+    active()
 });
 
 
@@ -40,94 +52,107 @@ var button2 = document.querySelector('#button2');
 var cancel = document.querySelector('.cancel');
 var form = document.querySelector('.form');
 
-if(button1)
-button1.addEventListener('click', function(){
-    form.style.display = 'flex';
-});
+if (button1)
+    button1.addEventListener('click', function () {
+        form.style.display = 'flex';
+    });
 
-if(cancel)
-cancel.addEventListener('click', function(){
-    form.style.display = 'none';
-})
+if (cancel)
+    cancel.addEventListener('click', function () {
+        form.style.display = 'none';
+    })
 
+function active() {
+    let button3 = document.querySelector('.button3');
+    let button4 = document.querySelector('.button4');
+    let button5 = document.querySelector('.button5');
+    let button6 = document.querySelector('.button6');
+    let button7 = document.querySelector('.button7');
+    let if3 = document.querySelector('#if3');
+    let if4 = document.querySelector('#if4');
+    let if5 = document.querySelector('#if5');
+    let if6 = document.querySelector('#if6');
+    let if7 = document.querySelector('#if7');
+    var cancel3 = document.querySelector('#cancel3 i');
+    var cancel4 = document.querySelector('#cancel4 i');
+    var cancel5 = document.querySelector('#cancel5 i');
+    var cancel6 = document.querySelector('#cancel6 i');
+    var cancel7 = document.querySelector('#cancel7 i');
 
-var button3 = document.getElementById('button3');
-var button4 = document.getElementById('button4');
-var button5 = document.getElementById('button5');
-var button6 = document.getElementById('button6');
-var button7 = document.getElementById('button7');
-var if3 = document.querySelector('#if3');
-var if4 = document.querySelector('#if4');
-var if5 = document.querySelector('#if5');
-var if6 = document.querySelector('#if6');
-var if7 = document.querySelector('#if7');
-var cancel3 = document.querySelector('#cancel3 i');
-var cancel4 = document.querySelector('#cancel4 i');
-var cancel5 = document.querySelector('#cancel5 i');
-var cancel6 = document.querySelector('#cancel6 i');
-var cancel7 = document.querySelector('#cancel7 i');
-var body = document.querySelector('body');
+    var a_button3 = document.querySelectorAll(".button3");
+    a_button3 = Array.from(a_button3);
+    a_button3.map(value => {
+        value.addEventListener('click', function () {
+            if3.style.display = 'flex';
+            body.style.overflow = 'hidden';
+        });
+    })
 
-if(button3)
-button3.addEventListener('click', function(){
-    if3.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
+    if (cancel3)
+        cancel3.addEventListener('click', function () {
+            if3.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
 
-if(cancel3)
-cancel3.addEventListener('click', function(){
-    if3.style.display = 'none';
-    body.style.overflow = 'auto';
-})
+    var a_button4 = document.querySelectorAll(".button4");
+    a_button4 = Array.from(a_button4);
+    a_button4.map(value => {
+        value.addEventListener('click', function () {
+            if4.style.display = 'flex';
+            body.style.overflow = 'hidden';
+            console.log("ehllo")
+        });
+    })
 
+    if (cancel4)
+        cancel4.addEventListener('click', function () {
+            if4.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
 
-if(button4)
-button4.addEventListener('click', function(){
-    if4.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
+    var a_button5 = document.querySelectorAll(".button5");
+    a_button5 = Array.from(a_button5);
+    a_button5.map(value => {
+        value.addEventListener('click', function () {
+            if5.style.display = 'flex';
+            body.style.overflow = 'hidden';
+        });
+    })
 
-if(cancel4)
-cancel4.addEventListener('click', function(){
-    if4.style.display = 'none';
-    body.style.overflow = 'auto';
-})
+    if (cancel5)
+        cancel5.addEventListener('click', function () {
+            if5.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
 
+    var a_button6 = document.querySelectorAll(".button6");
+    a_button6 = Array.from(a_button6);
+    a_button6.map(value => {
+        value.addEventListener('click', function () {
+            if6.style.display = 'flex';
+            body.style.overflow = 'hidden';
+        });
+    })
 
-if(button5)
-button5.addEventListener('click', function(){
-    if5.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
+    if (cancel6)
+        cancel6.addEventListener('click', function () {
+            if6.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
 
-if(cancel5)
-cancel5.addEventListener('click', function(){
-    if5.style.display = 'none';
-    body.style.overflow = 'auto';
-})
+    var a_button7 = document.querySelectorAll(".button7");
+    a_button7 = Array.from(a_button7);
+    a_button7.map(value => {
+        value.addEventListener('click', function () {
+            if7.style.display = 'flex';
+            body.style.overflow = 'hidden';
+        });
+    })
 
+    if (cancel7)
+        cancel7.addEventListener('click', function () {
+            if7.style.display = 'none';
+            body.style.overflow = 'auto';
+        })
 
-if(button6)
-button6.addEventListener('click', function(){
-    if6.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
-
-if(cancel6)
-cancel6.addEventListener('click', function(){
-    if6.style.display = 'none';
-    body.style.overflow = 'auto';
-})
-
-
-if(button7)
-button7.addEventListener('click', function(){
-    if7.style.display = 'flex';
-    body.style.overflow = 'hidden';
-});
-
-if(cancel7)
-cancel7.addEventListener('click', function(){
-    if7.style.display = 'none';
-    body.style.overflow = 'auto';
-})
+}
