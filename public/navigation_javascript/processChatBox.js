cffe08d327c6191c60b70497451977e4261faf86
truong <lst27062004@gmail.com>
2024-04-04 04:45:05 +0700

    fix processChatBox.js

--- public/navigation_javascript/processChatBox.js
+++ public/navigation_javascript/processChatBox.js
@@ -1,4 +1,4 @@
-import { scrollToBottom } from "../src/function.js";
+// import { scrollToBottom } from "../src/function.js";
 
 const purple = document.querySelector('.color.purple');
 const pink = document.querySelector('.color.pink');
@@ -12,37 +12,42 @@ setTimeout(() => {
     }, 500);
 }, 500);
 
-const buttonOpenChat = document.querySelector('.button-open-chat');
-const buttonCloseChat = document.querySelector('.chat-minimize');
-const chatBox = document.querySelector('.chat-box');
-const chatBoxWrapper = document.querySelector('.chat-box-wrapper');
 
-buttonOpenChat.onclick = function () {
-    chatBox.style.display = 'flex';
-    chatBoxWrapper.style.animation = 'flyToTopChatBox .3s ease-in-out forwards';
-    html.style.overflow = 'hidden';
-    scrollToBottom();
-}
-buttonCloseChat.onclick = function () {
-    chatBoxWrapper.style.animation = 'fadeOutChatBox .3s ease-in-out forwards';
-    setTimeout(() => {
-        chatBox.style.display = 'none';
-        html.style.overflow = 'auto';
-    }, 300);
-}
-
-const showButtonImage = document.querySelector('.plus');
-const buttonImageWrapper = document.querySelector('.button-image-wrapper');
-
-showButtonImage.onclick = function() {
-    let widthButtonImageWrapper = document.querySelector('.button-image-wrapper').offsetWidth;
-    if (widthButtonImageWrapper === 30) {
-        console.log('hihi');
-        buttonImageWrapper.style.animation = 'hideButtonImage .3s ease-in-out forwards';
-        showButtonImage.style.transform = 'rotate(0deg)';
-    } else {
-        console.log('hiha');
-        buttonImageWrapper.style.animation = 'showButtonImage .3s ease-in-out forwards';
-        showButtonImage.style.transform = 'rotate(45deg)';
+$(document).ready(function(){
+    
+    const html = document.querySelector('html');
+    const buttonOpenChat = document.querySelector('.button-open-chat');
+    const buttonCloseChat = document.querySelector('.chat-minimize');
+    const chatBox = document.querySelector('.chat-box');
+    const chatBoxWrapper = document.querySelector('.chat-box-wrapper');
+    
+    buttonOpenChat.onclick = function () {
+        chatBox.style.display = 'flex';
+        chatBoxWrapper.style.animation = 'flyToTopChatBox .3s ease-in-out forwards';
+        html.style.overflow = 'hidden';
+        // scrollToBottom();
     }
-}
+    buttonCloseChat.onclick = function () {
+        chatBoxWrapper.style.animation = 'fadeOutChatBox .3s ease-in-out forwards';
+        setTimeout(() => {
+            chatBox.style.display = 'none';
+            html.style.overflow = 'auto';
+        }, 300);
+    }
+    
+    const showButtonImage = document.querySelector('.plus');
+    const buttonImageWrapper = document.querySelector('.button-image-wrapper');
+    
+    showButtonImage.onclick = function() {
+        let widthButtonImageWrapper = document.querySelector('.button-image-wrapper').offsetWidth;
+        if (widthButtonImageWrapper === 30) {
+            console.log('hihi');
+            buttonImageWrapper.style.animation = 'hideButtonImage .3s ease-in-out forwards';
+            showButtonImage.style.transform = 'rotate(0deg)';
+        } else {
+            console.log('hiha');
+            buttonImageWrapper.style.animation = 'showButtonImage .3s ease-in-out forwards';
+            showButtonImage.style.transform = 'rotate(45deg)';
+        }
+    }
+});
