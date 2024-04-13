ab94ebb1e043d42e4c09fa4e5d0f4d70f8511a1f
No-Ja-Wenn <ngogiaquyendhtn223@gmail.com>
2024-04-14 01:20:12 +0700

    Quyen commit

--- public/navigation_javascript/wheyshop_tracking.js
+++ public/navigation_javascript/wheyshop_tracking.js
@@ -11,9 +11,9 @@ a_tagbox.forEach((element, index)=>{
         allTag = Array.from(allTag);
         allTag.forEach((element, index) => {
 
-            element.classList.remove('tagBox1');
-            element.classList.remove('tagBox2');
-            element.classList.remove('tagBox3');
+            element.classList.remove('tagbox1');
+            element.classList.remove('tagbox2');
+            element.classList.remove('tagbox3');
             element.querySelector(".tag").classList.remove('tag1');
             element.querySelector(".tag").classList.remove('tag2');
             element.querySelector(".tag").classList.remove('tag3');
@@ -22,16 +22,16 @@ a_tagbox.forEach((element, index)=>{
             element.querySelector(".title").classList.remove('title3');
         });
         
-        allTag[index].classList.add('tagBox3');
+        allTag[index].classList.add('tagbox3');
         allTag[index].querySelector(".tag").classList.add('tag3');
         allTag[index].querySelector(".title").classList.add('title3');
     
         
-        allTag[index-1].classList.add('tagBox1');
+        allTag[index-1].classList.add('tagbox1');
         allTag[index-1].querySelector(".tag").classList.add('tag1');
         allTag[index-1].querySelector(".title").classList.add('title1');
     
-        allTag[index+1].classList.add('tagBox2');
+        allTag[index+1].classList.add('tagbox2');
         allTag[index+1].querySelector(".tag").classList.add('tag2');
         allTag[index+1].querySelector(".title").classList.add('title2');
     })
